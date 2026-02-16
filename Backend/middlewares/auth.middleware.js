52e13e350805db763d0d7d3723db125e82d7003a
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2026-02-16 07:39:10 +0530

    Add authentication middleware for token verification

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+const getTokenFromRequest = (req) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader?.startsWith('Bearer ')) return authHeader.split(' ')[1];
+    return null;
+}
+
+const authMiddleware = (req, res, next) => {
+    try{
+        const token = getTokenFromRequest(req);
+
+        if (!token) return res.status(401).json({ message: "Unauthorized. Token not found.", error: true, success: false });
+        
+        const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            message: 
+                err.name === 'TokenExpiredError'
+                    ? 'Unauthorized. Token has expired.'
+                    : 'Unauthorized. Invalid token.',
+            error: true,
+            success: false
+        })
+    }
+}
